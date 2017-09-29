7d0765fff9246c4b6d84fbfca4f7f42b5dea30e8
N Virtseva <virtseva@list.ru>
2017-09-29 13:25:08 +0300

    first commit

---
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.user.myapplication;
+
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        EditText editText = (EditText) findViewById(R.id.plain_text_input);
+        editText.setText("text changed");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void changeLanguage(String locale) {
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        Resources resources = getResources();
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
+        recreate();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int item_id = item.getItemId();
+        if (item_id == R.id.action_settings) {
+            Toast.makeText(this, "Setting menu item was clicked", Toast.LENGTH_LONG).show();
+
+
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
